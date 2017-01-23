0aa105a8e70ab4bd699f2fbfbac0c68c9674889e
birdbird314 <grantstadt@gmail.com>
2017-01-23 19:05:03 +0100

    implement CaseParser

--- src/main/java/birdbird314/google_code_jams/case_solver/CaseParser.java
+++ src/main/java/birdbird314/google_code_jams/case_solver/CaseParser.java
@@ -2,6 +2,8 @@ package birdbird314.google_code_jams.case_solver;
 
 import birdbird314.google_code_jams.rotate.GameBoard;
 
+import java.util.StringTokenizer;
+
 import static birdbird314.google_code_jams.rotate.GameBoard.BLUE;
 import static birdbird314.google_code_jams.rotate.GameBoard.EMPTY;
 import static birdbird314.google_code_jams.rotate.GameBoard.RED;
@@ -10,15 +12,54 @@ import static birdbird314.google_code_jams.rotate.GameBoard.RED;
  * Created by Kamil on 2017-01-23.
  */
 class CaseParser {
-    TestCase parse(String stringTestCase) {
-        char[][] gameboardArray = {
-                {RED  , EMPTY, EMPTY, EMPTY},
-                {BLUE , RED  , EMPTY, EMPTY},
-                {BLUE , RED  , EMPTY, EMPTY},
-                {BLUE , RED  , EMPTY, EMPTY}
-        };
-        GameBoard gameBoard = new GameBoard(gameboardArray);
+    private StringTokenizer lineTokenizer;
+    private int K, N;
 
-        return new TestCase(gameBoard, 3);
+    TestCase parse(String stringTestCase) {
+        lineTokenizer = new StringTokenizer(stringTestCase, "\n");
+
+        String firstLine = lineTokenizer.nextToken();
+        extractKandNFromFirstLine(firstLine);
+
+        GameBoard gameBoard = extractGameBoard();
+
+        return new TestCase(gameBoard, K);
     }
+
+    private GameBoard extractGameBoard() {
+        char[][] gameBoardArray = new char[N][N];
+
+        for(int i=0; i<N; i++)
+            gameBoardArray[i] = extractLine(lineTokenizer.nextToken());
+
+        return new GameBoard(gameBoardArray);
+    }
+
+    private void extractKandNFromFirstLine(String firstLine) {
+        StringTokenizer tokenizer = new StringTokenizer(firstLine, " ");
+
+        N = Integer.parseInt(tokenizer.nextToken());
+        K = Integer.parseInt(tokenizer.nextToken());
+    }
+
+    private char[] extractLine(String line) {
+        char[] result = new char[N];
+
+        for(int i=0; i<N; i++) {
+            switch (line.charAt(i)) {
+                case '.':
+                    result[i] = GameBoard.EMPTY;
+                    break;
+                case 'B':
+                    result[i] = GameBoard.BLUE;
+                    break;
+                case 'R':
+                    result[i] = GameBoard.RED;
+                    break;
+            }
+        }
+
+        return result;
+    }
+
 }
